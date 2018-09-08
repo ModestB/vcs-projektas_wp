5b4da39f6718f1eefb12259d208a7cc3f123e33a
Modestas Bujanauskas <mbujanauskas@gmail.com>
2018-09-08 09:19:05 +0300

    Add homepage template

--- footer.php
+++ footer.php
@@ -8,9 +8,10 @@
             <div class="contact flex-container flex--wrap flex--start">
                 <!-- Start of Send mail form-->
                 <div class="send-mail col-1-2"> 
-                    <form action="mailto:Mbujanauskas@gmail.com" method='post' enctype="text/plain">
+                    <!-- <form action="mailto:Mbujanauskas@gmail.com" method='post' enctype="text/plain"> -->
                         <div class="flex-container flex--column">
-                            <label for="name">Your name </label>
+                        <?php echo do_shortcode(get_field('cup_short_code'))?>
+                            <!-- <label for="name">Your name </label>
                             <input type="text" id="name" placeholder="Enter your name">
                             <label for="email">Email </label>
                             <input type="text" id="email" placeholder="Enter your email">
@@ -18,9 +19,9 @@
                             <input type="text" id="subject" placeholder="Enter message subject">
                             <label for="email-text">Message </label>
                             <textarea name="email-text" id="email-text" placeholder="Enter your message"></textarea>
-                            <button type="submit" class="flex-container">Send <i class="icon-arrow-right2"></i></button>
+                            <button type="submit" class="flex-container">Send <i class="icon-arrow-right2"></i></button> -->
                         </div>
-                    </form>
+                    <!-- </form> -->
                 </div> 
                
                 <div class="contact__details flex-container flex--wrap col-1-2">
---
+++ templates/homepage.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Template Name: Homepage Template */
+
+get_header();
+
+?>
+<h1>Esame homepage.php</h1>
+<?php 
+   
+    
+?>
+
+<?php get_footer(); ?>
